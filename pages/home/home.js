35ecb77d483f45d94bec0835588ef57b83109a06
Migueks <lhlmiguel@gmail.com>
2024-09-28 11:53:45 +0200

    ultima cosa

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,7 +8,7 @@ export const Home = () => {
     <section class="home">
     <p>Hey, I'm</p>
     <h1>Miguel López-Herrero</h1>
-    <p>I'm a software engineer based in Toronto, Canada and also a communication and journalism student. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</p>
+    <p>I'm a software engineer based in Málaga, Spain. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</p>
     <a href="mailto:lhlmiguel@gmail.com">Say hi →</a>
     </section>`;
 };
